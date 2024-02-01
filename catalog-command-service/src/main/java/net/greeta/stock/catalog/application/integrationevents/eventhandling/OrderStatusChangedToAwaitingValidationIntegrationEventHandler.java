01a4df4bb326de727d1fe17728f3a677eaa95f17
skyglass <skyglass2001@gmail.com>
2024-02-02 00:54:33 +0100

    continue concurrency test implementation: fixed stock removal for paid orders

--- catalog-command-service/src/main/java/net/greeta/stock/catalog/application/integrationevents/eventhandling/OrderStatusChangedToAwaitingValidationIntegrationEventHandler.java
+++ catalog-command-service/src/main/java/net/greeta/stock/catalog/application/integrationevents/eventhandling/OrderStatusChangedToAwaitingValidationIntegrationEventHandler.java
@@ -1,14 +1,11 @@
 package net.greeta.stock.catalog.application.integrationevents.eventhandling;
 
 import lombok.RequiredArgsConstructor;
-import net.greeta.stock.catalog.application.commandbus.CatalogCommandBus;
 import net.greeta.stock.catalog.application.integrationevents.IntegrationEventPublisher;
 import net.greeta.stock.catalog.application.integrationevents.events.*;
 import net.greeta.stock.catalog.config.KafkaTopics;
 import net.greeta.stock.catalog.domain.catalogitem.CatalogItem;
 import net.greeta.stock.catalog.domain.catalogitem.CatalogItemRepository;
-import net.greeta.stock.common.domain.dto.catalog.AddStockCommand;
-import net.greeta.stock.common.domain.dto.catalog.RemoveStockCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -26,11 +23,10 @@ public class OrderStatusChangedToAwaitingValidationIntegrationEventHandler {
   private final CatalogItemRepository catalogItemRepository;
   private final IntegrationEventPublisher integrationEventService;
   private final KafkaTopics kafkaTopics;
-  private final CatalogCommandBus commandBus;
 
   @KafkaListener(
-      groupId = "${app.kafka.group.ordersWaitingValidation}",
-      topics = "${spring.kafka.consumer.topic.ordersWaitingForValidation}"
+          groupId = "${app.kafka.group.ordersWaitingValidation}",
+          topics = "${spring.kafka.consumer.topic.ordersWaitingForValidation}"
   )
   @Transactional
   public void handle(OrderStatusChangedToAwaitingValidationIntegrationEvent event) {
@@ -38,53 +34,21 @@ public class OrderStatusChangedToAwaitingValidationIntegrationEventHandler {
     var confirmedOrderStockItems = new ArrayList<ConfirmedOrderStockItem>();
 
     event.getOrderStockItems().forEach(orderStockItem -> catalogItemRepository
-        .load(orderStockItem.getProductId())
-        .ifPresent(catalogItem ->
-            confirmedOrderStockItems.add(createConfirmedOrderStockItem(catalogItem, orderStockItem)))
+            .load(orderStockItem.getProductId())
+            .ifPresent(catalogItem ->
+                    confirmedOrderStockItems.add(createConfirmedOrderStockItem(catalogItem, orderStockItem)))
     );
 
-    boolean allItemsAvailable = allItemsAvailable(confirmedOrderStockItems);
-    if (allItemsAvailable) {
-      var processedStockItems = new ArrayList<OrderStockItem>();
-      allItemsAvailable = processItems(processedStockItems, event);
-      if (allItemsAvailable) {
-        integrationEventService.publish(
-                kafkaTopics.getOrderStockConfirmed(),
-                new OrderStockConfirmedIntegrationEvent(event.getOrderId()));
-      } else {
-        rollbackProcessedItems(processedStockItems);
-      }
-    }
-    if (!allItemsAvailable) {
+    if (allItemsAvailable(confirmedOrderStockItems)) {
+      integrationEventService.publish(
+              kafkaTopics.getOrderStockConfirmed(),
+              new OrderStockConfirmedIntegrationEvent(event.getOrderId())
+      );
+    } else {
       integrationEventService.publish(kafkaTopics.getOrderStockRejected(),
-          new OrderStockRejectedIntegrationEvent(event.getOrderId(), confirmedOrderStockItems));
+              new OrderStockRejectedIntegrationEvent(event.getOrderId(), confirmedOrderStockItems));
     }
 
-  }
-
-  private boolean processItems(List<OrderStockItem> processedStockItems, OrderStatusChangedToAwaitingValidationIntegrationEvent event) {
-    for (OrderStockItem orderItem: event.getOrderStockItems()) {
-      RemoveStockCommand command = new RemoveStockCommand(orderItem.getProductId(), orderItem.getUnits());
-      try {
-        commandBus.execute(command);
-        processedStockItems.add(orderItem);
-      } catch (Exception e) {
-        return false;
-      }
-    };
-    return true;
-  }
-
-  private boolean rollbackProcessedItems(List<OrderStockItem> processedStockItems) {
-    for (OrderStockItem orderItem: processedStockItems) {
-      AddStockCommand command = new AddStockCommand(orderItem.getProductId(), orderItem.getUnits());
-      try {
-        commandBus.execute(command);
-      } catch (Exception e) {
-        //TODO: implement retry and DLQ
-      }
-    };
-    return true;
   }
 
   private ConfirmedOrderStockItem createConfirmedOrderStockItem(CatalogItem catalogItem, OrderStockItem orderStockItem) {
--- catalog-command-service/src/main/java/net/greeta/stock/catalog/application/integrationevents/eventhandling/OrderStatusChangedToPaidIntegrationEventHandler.java
+++ catalog-command-service/src/main/java/net/greeta/stock/catalog/application/integrationevents/eventhandling/OrderStatusChangedToPaidIntegrationEventHandler.java
@@ -1,9 +1,12 @@
 package net.greeta.stock.catalog.application.integrationevents.eventhandling;
 
+import net.greeta.stock.catalog.application.commandbus.CatalogCommandBus;
 import net.greeta.stock.catalog.application.integrationevents.events.OrderStatusChangedToPaidIntegrationEvent;
+import net.greeta.stock.catalog.application.integrationevents.events.OrderStockItem;
 import net.greeta.stock.catalog.domain.catalogitem.CatalogItemRepository;
 import net.greeta.stock.catalog.domain.catalogitem.Units;
 import lombok.AllArgsConstructor;
+import net.greeta.stock.common.domain.dto.catalog.RemoveStockCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -16,14 +19,15 @@ public class OrderStatusChangedToPaidIntegrationEventHandler {
 
   private final CatalogItemRepository catalogItemRepository;
 
+  private final CatalogCommandBus commandBus;
+
   @KafkaListener(groupId = "${app.kafka.group.paidOrders}", topics = "${spring.kafka.consumer.topic.paidOrders}")
   public void handle(OrderStatusChangedToPaidIntegrationEvent event) {
     logger.info("Handling integration event: {} ({})", event.getId(), event.getClass().getSimpleName());
-    event.getOrderStockItems().forEach(orderStockItem -> catalogItemRepository
-        .load(orderStockItem.getProductId())
-        .ifPresent(catalogItem ->
-            catalogItem.removeStock(Units.of(orderStockItem.getUnits()))
-        )
-    );
+    for (OrderStockItem orderItem: event.getOrderStockItems()) {
+      RemoveStockCommand command = new RemoveStockCommand(orderItem.getProductId(), orderItem.getUnits());
+      final var response = commandBus.execute(command);
+      logger.info("RemoveStockCommand for product {} with version {}", response.getProductId(), response.getVersion());
+    };
   }
 }
